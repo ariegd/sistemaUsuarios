7b6f105d68f14f04208fa35309051029996451fe
ariegd <ariegd4@gmail.com>
2017-07-29 12:07:33 +0200

    modificando el fichero conexion.php

--- funcs/conexion.php
+++ funcs/conexion.php
@@ -1,10 +1,11 @@
 <?php
 	
-	$mysqli=new mysqli("localhost","root","password","login"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	
+	$mysqli=new mysqli("localhost","zodd","nbuser","login"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
 		exit();
 	}
-	
+
+echo 'Conexion Exitosa';
 ?>
