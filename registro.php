b6dfa8bc9113dc09fca4c71da001ac23ab62ebf9
ariegd <ariegd4@gmail.com>
2017-07-29 12:57:46 +0200

    modificado 12:57

--- registro.php
+++ registro.php
@@ -1,3 +1,118 @@
+<?php
+    /**
+    * Es como un include
+    */
+    require 'funcs/conexion.php';
+    include 'funcs/funcs.php';
+
+    /**
+     * Aquí vamos a ir colocando todos los errores
+     */
+    $errors = array();
+
+    /**
+     * Cuando nos envien un POST con el boton, lo validamos dentro del if
+     */
+    if(!empty($_POST))
+    {
+        /**
+         * Aquí recogemos todos los elementos del formulario y lo guardamos dentro de variables.
+         * Con la función "real_escape_string" limpiamos la cadena que recibimos del formulario
+         * y así evitamos el sql_injection.
+         */
+        $nombre = $mysqli->real_escape_string($_POST['nombre']);
+        $usuario = $mysqli->real_escape_string($_POST['usuario']);
+        $password = $mysqli->real_escape_string($_POST['password']);
+        $con_password = $mysqli->real_escape_string($_POST['con_password']);
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $captcha = $mysqli->real_escape_string($_POST['g-recaptcha-response']);
+
+        /**
+         * $activo cuando registremos el usuario que siempre esté desactivado
+         * $tipo_usuario indica los privilegios para este usuario, van a ser user estandar
+         * $secret colocar la clave secreta del catch
+         */
+        $activo = 0;
+        $tipo_usuario = 2;
+        $secret = 'clave secreta de reCaptcha';//Modificar
+
+        /**
+         * A partir de aquí comenzamos con las validaciones de cada una de las variables
+         * Como se puede ver las validaciones utilizan funciones ya implementadas en funcs.php
+         */
+        if(!$captcha){
+            $errors[] = "Por favor verifica el captcha";
+        }
+
+        if(isNull($nombre, $usuario, $password, $con_password, $email))
+        {
+            $errors[] = "Debe llenar todos los campos";
+        }
+
+        if(!isEmail($email))
+        {
+            $errors[] = "Dirección de correo inválida";
+        }
+
+        if(!validaPassword($password, $con_password))
+        {
+            $errors[] = "Las contraseñas no coinciden";
+        }
+
+        if(usuarioExiste($usuario))
+        {
+            $errors[] = "El nombre de usuario $usuario ya existe";
+        }
+
+        if(emailExiste($email))
+        {
+            $errors[] = "El correo electronico $email ya existe";
+        }
+
+        /**
+         * Una vez terminado con las validaciones vamos a mostrar los resultados
+         * y guardarlo en la la base de datos.
+         */
+        if(count($errors) == 0)
+        {
+
+            $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$captcha");
+
+            $arr = json_decode($response, TRUE);
+
+            if($arr['success'])
+            {
+
+                $pass_hash = hashPassword($password);
+                $token = generateToken();
+
+                $registro = registraUsuario($usuario, $pass_hash, $nombre, $email, $activo, $token, $tipo_usuario);
+                if($registro > 0)
+                {
+                    $url = 'http://'.$_SERVER["SERVER_NAME"].'/login/activar.php?id='.$registro.'&val='.$token;
+
+                    $asunto = 'Activar Cuenta - Sistema de Usuarios';
+                    $cuerpo = "Estimado $nombre: <br /><br />Para continuar con el proceso de registro, es indispensable de click en la siguiente liga <a href='$url'>Activar Cuenta</a>";
+
+                    if(enviarEmail($email, $nombre, $asunto, $cuerpo)){
+
+                        echo "Para terminar el proceso de registro siga las instrucciones que le hemos enviado la direccion de correo electronico: $email";
+                        echo "<br><a href='index.php' >Iniciar Sesion</a>";
+                        exit;
+                    } else {
+                        $erros[] = "Error al enviar Email";
+                    }
+
+                } else {
+                    $errors[] = "Error al Registrar";
+                }
+
+            } else {
+                $errors[] = 'Error al comprobar Captcha';
+            }
+        }
+    }
+?>
 <html>
 	<head>
 		<title>Registro</title>
